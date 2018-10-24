edb3152399826b36be949ffa3142283b9c1650d2
Beiatrix Pedrasa <bpedrasa@gmail.com>
2018-10-24 14:26:35 -0400

    10-24-2018 closed react-bulletin - broken

--- pages/15-bulletin/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Board from './Board';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Board count={50}/>, document.getElementById('root'));
-registerServiceWorker();
