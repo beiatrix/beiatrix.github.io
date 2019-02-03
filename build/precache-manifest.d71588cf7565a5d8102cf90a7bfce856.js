f607a76478cae1b98ce17a00a67068136a9020f0
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-02-03 17:57:59 -0500

    init react app

---
+++ build/precache-manifest.d71588cf7565a5d8102cf90a7bfce856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7b4384a77540eaa6dd8",
+    "url": "/static/js/main.a7b4384a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a7b4384a77540eaa6dd8",
+    "url": "/static/css/main.2171dbb8.chunk.css"
+  },
+  {
+    "revision": "acd96dc40a8749b9e13de82983f4c8e9",
+    "url": "/index.html"
+  }
+];
